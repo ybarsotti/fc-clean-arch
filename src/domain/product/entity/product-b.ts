7f58d9d716de3f15c5c10a32876362c5d40b365f
Yuri Barsotti <yuri@weef.com.br>
2023-02-07 20:54:53 -0300

    feat(product): adiciona notification pattern em produto
    Adiciona testes com novas validações

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,11 +1,12 @@
 import ProductInterface from "./product.interface";
+import Entity from "../../@shared/entity/entity.abstract";
 
-export default class ProductB implements ProductInterface {
-  private _id: string;
+export default class ProductB extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
@@ -34,16 +35,24 @@ export default class ProductB implements ProductInterface {
     this.validate();
   }
 
-  validate(): boolean {
+  validate() {
     if (this._id.length === 0) {
-      throw new Error("Id is required");
+      this.notification.addError({
+        context: 'product',
+        message: 'Id is required',
+      })
     }
     if (this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({
+        context: 'product',
+        message: 'Name is required',
+      })
     }
     if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
+      this.notification.addError({
+        context: 'product',
+        message: 'Price must be greater than zero',
+      })
     }
-    return true;
   }
 }
